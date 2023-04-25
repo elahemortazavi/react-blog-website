ecafbbbd03377fc8515f29f913d16eb9bb368bf2
elahemortazavi <emor5680@gmail.com>
2023-04-25 19:47:18 +0100

    post.jsx & css

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router";
 import Header from "../../components/header/Header";
-import Posts from "../../components/posts/Posts";
+import Posts from "../../components/post/Post";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./homepage.css";
 
